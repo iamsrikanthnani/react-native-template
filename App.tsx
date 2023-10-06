738a7de94a0bf4780eab2c4ad27f505b52950026
srikanthnani <srikanthnani1202@gmail.com>
2023-10-06 23:15:23 +0530

    iOS codepush setup :wrench:

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import {View, Text} from 'react-native';
+import {View, Text, Platform} from 'react-native';
 import React, {useEffect} from 'react';
 import Config from 'react-native-config';
 type Props = {};
 import Analytics from 'appcenter-analytics';
+import codePush from 'react-native-code-push';
 const App = (props: Props) => {
   useEffect(() => {
     Analytics.trackEvent('My custom event, from ' + Config.ENV);
@@ -15,4 +16,15 @@ const App = (props: Props) => {
   );
 };
 
-export default App;
+// Define the deployment key based on the platform (iOS or Android)
+const deploymentKey = Platform.select({
+  ios: Config.IOS_CODEPUSH_DEPLOYMENT_KEY,
+});
+
+// Wrap your App component with CodePush higher-order component
+const CodePushedApp = codePush({
+  deploymentKey,
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(App);
+
+export default CodePushedApp;
